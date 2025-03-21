21529447f81b6c0428560c31e3814b5b1a8fd88b
Shujat Ali <Shujat.dev@gmail.com>
2025-03-21 16:05:25 -0500

    Initial Commit

---
+++ CustomFramework/SDK.swift
@@ -0,0 +1,45 @@
+//
+//  SDK.swift
+//  CustomFramework
+//
+//  Created by Shujat Ali on 4/12/24.
+//
+
+
+
+import Foundation
+
+public struct SDK {
+    
+    private static var isAPIKeyValid = false
+    
+    private static let apiKeys = [
+        "privateKey123"
+    ]
+    
+    private static func checkAPIKey() -> Bool {
+        if !isAPIKeyValid {
+            print("Please provide a valid API Key")
+        }
+        return isAPIKeyValid
+    }
+    
+    public static func configure(apiKey: String) {
+        isAPIKeyValid = apiKeys.contains(apiKey)
+    }
+    
+    public static func doSomeWork() {
+        guard checkAPIKey() else { return }
+        print("Doing some work...")
+    }
+    
+    public static func testSDKS() {
+        #if canImport(PLCrashReporter)
+        print("PLCrashReporter is available")
+        #endif
+
+        #if canImport(CocoaLumberjack)
+        print("CocoaLumberjack is available")
+        #endif
+    }
+}
---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Shujat Ali on 3/21/25.
+//
+
+import UIKit
+import CustomFramework
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        SDK.testSDKS()
+    }
+
+
+}
+
